470338a02e4131aefe905b4dbd7568bf0c635fc3
Cyrus Stoller <cyrus.stoller@gmail.com>
2014-03-26 16:29:59 -0700

    Adding default admin user and pass for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+  ENV["ADMIN_USER"] ||= "user"
+  ENV["ADMIN_PASS"] ||= "pass"
+
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
